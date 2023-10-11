645b51e95b53c30cff1efc133128085b74d8da7e
Alexis Willis <alexis.willis.dev@gmail.com>
2023-10-11 01:08:19 -0700

    initial

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Add Book</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, dicta
+      quibusdam quis commodi perferendis possimus illo hic, quia delectus
+      ducimus placeat, at eaque dolorem mollitia odio nulla soluta odit
+      temporibus.
+    </p>
+  </div>
 </template>
 
 <script setup></script>
